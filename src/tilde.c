631f8cd8321bcd23a8889459f19de4966d2efbad
Ian Beckwith <ianb@erislabs.net>
2008-02-15 21:43:00 +0000

    updated license info

--- src/tilde.c
+++ src/tilde.c
@@ -6,9 +6,9 @@
    of text with interactive input and history editing.
 
    Readline is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the
-   Free Software Foundation; either version 3, or (at your option) any
-   later version.
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
 
    Readline is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,8 +16,8 @@
    General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with Readline; see the file COPYING.  If not, write to the Free
-   Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
+   along with Readline; see the file COPYING.  If not, see
+   http://www.gnu.org/licenses/ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
